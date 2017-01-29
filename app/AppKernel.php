30828d0986dac812fb2d3492f1e5e6fae0141aa9
freezy <marek.viger@gmail.com>
2017-01-29 23:24:04 +0100

    Added KnpMenuBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,6 +16,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new DMS\Bundle\TwigExtensionBundle\DMSTwigExtensionBundle(),
+            new Knp\Bundle\MenuBundle\KnpMenuBundle(),
             new Maba\Bundle\WebpackBundle\MabaWebpackBundle(),
             new AppBundle\AppBundle(),
         ];
