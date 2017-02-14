a5881ff27a4a42377aafc34ca9d7d18618f63595
freezy <marek.viger@gmail.com>
2017-02-15 00:45:52 +0100

    Find active friends

--- src/AppBundle/Entity/Repository/UserRepository.php
+++ src/AppBundle/Entity/Repository/UserRepository.php
@@ -8,6 +8,35 @@ use Doctrine\ORM\EntityRepository;
 class UserRepository extends EntityRepository
 {
     /**
+     * Find all active users followed by user
+     *
+     * @param int $userId
+     *
+     * @return User[]
+     */
+    public function findActiveFollowing($userId)
+    {
+        $dql = sprintf(
+            'SELECT u FROM %s u
+JOIN %s ur WITH ur.nodeId = u.id
+WHERE ur.userId = :userId
+  AND ur.type = :type
+  AND u.lastAction IS NOT NULL
+ORDER BY u.login ASC',
+            User::class,
+            UserRelation::class
+        );
+
+        $query = $this->_em->createQuery($dql)
+            ->setParameters([
+                'userId' => $userId,
+                'type' => UserRelation::TYPE_FRIEND,
+            ]);
+
+        return $query->getResult();
+    }
+
+    /**
      * Find all user's followers
      *
      * @param int $userId
@@ -20,7 +49,8 @@ class UserRepository extends EntityRepository
             'SELECT u FROM %s u
 JOIN %s ur WITH ur.userId = u.id
 WHERE ur.nodeId = :userId
-  AND ur.type = :type',
+  AND ur.type = :type
+ORDER BY u.login ASC',
             User::class,
             UserRelation::class
         );
@@ -47,7 +77,8 @@ WHERE ur.nodeId = :userId
             'SELECT u FROM %s u
 JOIN %s ur WITH ur.nodeId = u.id
 WHERE ur.userId = :userId
-  AND ur.type = :type',
+  AND ur.type = :type
+ORDER BY u.login ASC',
             User::class,
             UserRelation::class
         );
