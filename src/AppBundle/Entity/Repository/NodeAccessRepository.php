5b34096a106dceb0a8a31c0444c4e863df059c7d
freezy <marek.viger@gmail.com>
2017-02-24 16:35:38 +0100

    Bookmarks service and controller

--- src/AppBundle/Entity/Repository/NodeAccessRepository.php
+++ src/AppBundle/Entity/Repository/NodeAccessRepository.php
@@ -1,9 +1,29 @@
 <?php
 namespace AppBundle\Entity\Repository;
 
-use Doctrine\ORM\EntityRepository;
+use AppBundle\Entity\NodeAccess;
 
-class NodeAccessRepository extends EntityRepository
+class NodeAccessRepository extends AbstractEntityRepository
 {
+    /**
+     * @param int $userId
+     *
+     * @return NodeAccess[]
+     */
+    public function findBookmarksByUserId($userId)
+    {
+        $query = $this->createQueryBuilder('na')
+            ->select('na', 'n', 'bc')
+            ->innerJoin('na.node', 'n')
+            ->leftJoin('na.bookmarkCategory', 'bc')
+            ->where('na.userId = :userId AND na.bookmark = :bookmark')
+            ->orderBy('n.name', 'ASC')
+            ->setParameters([
+                'bookmark' => NodeAccess::BOOKMARK_YES,
+                'userId' => $userId,
+            ])
+            ->getQuery();
 
+        return $query->getResult();
+    }
 }
